4e60ab98de1446a00f34ee6519116350f24e9c61
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-07-15 15:59:10 +0530

    functionality added

---
+++ index.js
@@ -0,0 +1,17 @@
+function toggleSignal(activeSignal) {
+    ['red', 'yellow', 'green'].forEach(signal => {
+        document.getElementById(`${signal}-signal`).classList.toggle('d-none', signal !== activeSignal);
+    });
+}
+
+document.getElementById('red-signal-button').addEventListener('click', function() {
+    toggleSignal('red');
+});
+
+document.getElementById('yellow-signal-button').addEventListener('click', function() {
+    toggleSignal('yellow');
+});
+
+document.getElementById('green-signal-button').addEventListener('click', function() {
+    toggleSignal('green');
+});
